45190e00a03b2945948eb8603f4b283ddcaf8b2c
Tanmay <tanmay.jha1@gmail.com>
2017-01-19 10:37:53 +0530

    Issues of splash screen and intro slider solved

--- app/src/main/java/com/example/tanmayjha/vitcabs/Entity/LogIn/LoginActivity.java
+++ app/src/main/java/com/example/tanmayjha/vitcabs/Entity/LogIn/LoginActivity.java
@@ -1,22 +1,36 @@
 package com.example.tanmayjha.vitcabs.Entity.LogIn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.tanmayjha.vitcabs.Entity.Navigation.HomeActivity;
 import com.example.tanmayjha.vitcabs.R;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView skipThisView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_in);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        skipThisView=(TextView)findViewById(R.id.skip_this);
+        skipThisView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/tanmayjha/vitcabs/Entity/SplashScreen/SplashScreenActivity.java
+++ app/src/main/java/com/example/tanmayjha/vitcabs/Entity/SplashScreen/SplashScreenActivity.java
@@ -1,25 +1,23 @@
 package com.example.tanmayjha.vitcabs.Entity.SplashScreen;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 
 import com.example.tanmayjha.vitcabs.Entity.IntroSlider.IntroSliderActivity;
 import com.example.tanmayjha.vitcabs.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
     private final int SPLASH_DISPLAY_DURATION = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Log.v("Splash Screen","Check 1");
+
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
