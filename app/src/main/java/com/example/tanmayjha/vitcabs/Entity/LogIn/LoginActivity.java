b89a57eda1434b956ab55209f6e6af78691eb7c0
Tanmay <tanmay.jha1@gmail.com>
2017-01-19 20:00:25 +0530

    Log in added

--- app/src/main/java/com/example/tanmayjha/vitcabs/Entity/LogIn/LoginActivity.java
+++ app/src/main/java/com/example/tanmayjha/vitcabs/Entity/LogIn/LoginActivity.java
@@ -1,21 +1,38 @@
 package com.example.tanmayjha.vitcabs.Entity.LogIn;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.tanmayjha.vitcabs.Entity.Navigation.HomeActivity;
 import com.example.tanmayjha.vitcabs.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 import org.w3c.dom.Text;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener {
+    String TAG=LoginActivity.class.getSimpleName();
+    public static GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+    private SignInButton buttonSignIn;
+    private static final int RC_SIGN_IN = 007;
 
     TextView skipThisView;
     @Override
@@ -25,13 +42,126 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         skipThisView=(TextView)findViewById(R.id.skip_this);
-        skipThisView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-            }
-        });
+        skipThisView.setOnClickListener(this);
+        buttonSignIn = (SignInButton) findViewById(R.id.button_sign_in);
+        buttonSignIn.setOnClickListener(this);
+        //Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
 
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        buttonSignIn.setSize(SignInButton.SIZE_STANDARD);
+        buttonSignIn.setScopes(gso.getScopeArray());
     }
 
+
+    private void signIn() {
+        //Maybe here signInIntent will get intent data from mGoogleApiClient
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        //Starting the intent prompts the user to select a Google account to sign in with.
+        // If you requested scopes beyond profile, email, and openid,
+        // the user is also prompted to grant access to the requested resources.
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "HandleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            //Signed in succesfilly, show authenticated UI
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Log.e(TAG, "display name:" + acct.getDisplayName());
+            String personName = acct.getDisplayName();
+            String personPhotoUrl;
+            if(acct.getPhotoUrl()!=null)
+            {
+                personPhotoUrl = acct.getPhotoUrl().toString();
+            }
+            else
+            {
+                personPhotoUrl="https://developers.google.com/experts/img/user/user-default.png";
+            }
+            Intent toMainActivity=new Intent(this, HomeActivity.class);
+            toMainActivity.putExtra("personName",personName);
+            toMainActivity.putExtra("personPhotoUrl",personPhotoUrl);
+            Log.e(TAG, "Name: " + personName + ",Image: " + personPhotoUrl);
+            startActivity(toMainActivity);
+        }
+    }
+
+    // onActivityResult() is called whenever user returns from Google Login UI.
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        //Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if(requestCode==RC_SIGN_IN){
+            GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
+    }
+
+    private void showProgressDialog(){
+        if(mProgressDialog==null){
+            mProgressDialog=new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog(){
+        if(mProgressDialog!=null && mProgressDialog.isShowing()){
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onClick(View v){
+        int id=v.getId();
+        switch (id)
+        {
+            case R.id.button_sign_in:
+                signIn();
+                break;
+            case R.id.skip_this:
+                Intent intent=new Intent(this,HomeActivity.class);
+                intent.putExtra("personName","Guest");
+                intent.putExtra("personPhotoUrl","");
+                startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/com/example/tanmayjha/vitcabs/Entity/Navigation/HomeActivity.java
+++ app/src/main/java/com/example/tanmayjha/vitcabs/Entity/Navigation/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.tanmayjha.vitcabs.Entity.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,11 +17,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.tanmayjha.vitcabs.Entity.LogIn.LoginActivity;
+import com.example.tanmayjha.vitcabs.Entity.Welcome.WelcomeFragment;
 import com.example.tanmayjha.vitcabs.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,GoogleApiClient.OnConnectionFailedListener {
+
+    String personName="User";
+    GoogleApiClient mGoogleApiClient;
+    String TAG=HomeActivity.class.getSimpleName();
+    String personPhotoUrl;
+    FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +68,51 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        WelcomeFragment timelineFragment=new WelcomeFragment();
+        ft.replace(R.id.container,timelineFragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+
+        Intent fromLogin=getIntent();
+        personName=fromLogin.getStringExtra("personName");
+        personPhotoUrl=fromLogin.getStringExtra("personPhotoUrl");
+        Log.v("Person's name",personName);
+        View hView =  navigationView.getHeaderView(0);
+        TextView name=(TextView)hView.findViewById(R.id.person_name);
+        CircleImageView personImage=(CircleImageView)hView.findViewById(R.id.person_image);
+        name.setText(personName);
+        Glide.with(getApplicationContext()).load(personPhotoUrl).thumbnail(0.5f).diskCacheStrategy(DiskCacheStrategy.ALL).into(personImage);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        String title="Timeline";
+        getSupportActionBar().setTitle(title);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        OptionalPendingResult<GoogleSignInResult> opr =
+                Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+
+        if (opr.isDone()) {
+            // Users cached credentials are valid, GoogleSignInResult containing ID token
+            // is available immediately. This likely means the current ID token is already
+            // fresh and can be sent to your server.
+            GoogleSignInResult result = opr.get();
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently and get a valid
+            // ID token. Cross-device single sign on will occur in this branch.
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult result) {
+                }
+            });
+        }
+
+        // Timeline code starts here onwards.!
     }
 
     @Override
@@ -82,7 +153,9 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_add_a_trip) {
+        if(id == R.id.nav_home){
+
+        } else if (id == R.id.nav_add_a_trip) {
             // Handle the camera action
         } else if (id == R.id.nav_pool_request) {
 
@@ -90,16 +163,55 @@ public class HomeActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_book_a_cab) {
 
-        } else if (id == R.id.nav_log_out) {
-
         } else if (id == R.id.nav_contact_us) {
 
-        }else if(id==R.id.nav_log_out){
-
         }
+        else if (id == R.id.nav_log_out){
+
+            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+                        }
+                    }
+            );
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
+
+        //TODO: Make function for fragment transaction
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mGoogleApiClient.stopAutoManage(this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.stopAutoManage(this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mGoogleApiClient.stopAutoManage(this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
